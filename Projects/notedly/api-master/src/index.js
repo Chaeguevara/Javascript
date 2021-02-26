f461abe074c66bbdceaaaa72176a73bbfdab6762
Chaeguevara <dfso2@naver.com>
2021-02-26 23:18:30 +0900

    Array as database and query

--- Projects/notedly/api-master/src/index.js
+++ Projects/notedly/api-master/src/index.js
@@ -7,30 +7,48 @@ const app = express();
 //port number
 const port = process.env.PORT || 4000;
 //Implement apollo-server for GraphQL
-const {ApolloServer, gql} = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const typeDefs = gql`
-type Query{
-    hello: String
-}
-`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String!
+    notes: [Note!]
+  }
+`;
+
+// array that will be resolved or mutated
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+// crate note type
+
 // Provide resolver functions for our schema fields
 const resolvers = {
-    Query:{
-        hello: () => 'Hello world!'
-    }
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes
+  }
 };
 
 // Apolo Server setup
-const server = new ApolloServer({typeDefs,resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 
 //Apply the Apollo GraphQL middleware and set the path to /api
-server.applyMiddleware({app,path:'/api'});
+server.applyMiddleware({ app, path: '/api' });
 
 //get methodd of app object. When user access the URL, get "Hello World"
-app.get('/',(req,res)=>res.send('Hello Web Server!!!'));
+app.get('/', (req, res) => res.send('Hello Web Server!!!'));
 
 //set port number
-app.listen(port,()=>
-    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
-    )
+app.listen(port, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
