a4c8fa5bb897b9b9f15f5f264989b1b40ade0345
Chaeguevara <dfso2@naver.com>
2021-02-27 15:33:31 +0900

    Mutation implemented

--- Projects/notedly/api-master/src/index.js
+++ Projects/notedly/api-master/src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 //Implement apollo-server for GraphQL
 const { ApolloServer, gql } = require('apollo-server-express');
+//schema
 const typeDefs = gql`
   type Note {
     id: ID!
@@ -17,6 +18,10 @@ const typeDefs = gql`
   type Query {
     hello: String!
     notes: [Note!]
+    note(id:ID!): Note!
+  }
+  type Mutation{
+    newNote(content: String!): Note!
   }
 `;
 
@@ -27,13 +32,28 @@ let notes = [
   { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
 ];
 
-// crate note type
+
 
 // Provide resolver functions for our schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    notes: () => notes
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent,args) => {
+      let noteValue = {
+        id: String(notes.length +1),
+        content: args.content,
+        author: 'Heejin Chae'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+
   }
 };
 
