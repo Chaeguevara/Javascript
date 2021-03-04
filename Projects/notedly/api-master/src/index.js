c5b7c3c6e9f10fc2b4b1244dbd94ae69c91e88e0
Chaeguevara <dfso2@naver.com>
2021-03-04 23:08:13 +0900

    Mongo DB implemented
     Local variables were replaced by MongoDB.

--- Projects/notedly/api-master/src/index.js
+++ Projects/notedly/api-master/src/index.js
@@ -1,70 +1,71 @@
 // index.js
 // This is the main entry point of our application
+
 // require express dependency
 const express = require('express');
-// add app object
-const app = express();
-//port number
-const port = process.env.PORT || 4000;
 //Implement apollo-server for GraphQL
 const { ApolloServer, gql } = require('apollo-server-express');
+//import env environment
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+
+//port number
+const port = process.env.PORT || 4000;
+//Store the DB_HOST value as a variable
+const DB_HOST = process.env.DB_HOST;
+
+
 //schema
 const typeDefs = gql`
   type Note {
-    id: ID!
-    content: String!
-    author: String!
+    id: ID
+    content: String
+    author: String
   }
   type Query {
-    hello: String!
-    notes: [Note!]
-    note(id:ID!): Note!
+    hello: String
+    notes: [Note]
+    note(id:ID): Note
   }
   type Mutation{
-    newNote(content: String!): Note!
+    newNote(content: String!): Note
   }
 `;
-
-// array that will be resolved or mutated
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
-];
-
 
 
 // Provide resolver functions for our schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args, id);
     }
   },
   Mutation: {
-    newNote: (parent,args) => {
-      let noteValue = {
-        id: String(notes.length +1),
+    newNote: async (parent, args) => {
+      return await models.Note, create({
         content: args.content,
         author: 'Heejin Chae'
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     }
-
   }
 };
+
+// add app object
+const app = express();
+
+db.connect(DB_HOST);
 
 // Apolo Server setup
 const server = new ApolloServer({ typeDefs, resolvers });
 
 //Apply the Apollo GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: '/api' });
-
-//get methodd of app object. When user access the URL, get "Hello World"
-app.get('/', (req, res) => res.send('Hello Web Server!!!'));
 
 //set port number
 app.listen(port, () =>
